3d230abe3e82e7a3b787e48b83dd58973327e25d
Test <testgyaan12@gmail.com>
2025-09-15 21:14:08 +0000

    Not view in mobile why

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -27,10 +27,18 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
+        <div className="mr-4 flex md:hidden">
+            <Link href="/" className="mr-6 flex items-center space-x-2">
+                <Code2 className="h-6 w-6" />
+                <span className="font-bold">
+                Novasuites
+                </span>
+            </Link>
+        </div>
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <Code2 className="h-6 w-6" />
-            <span className="hidden font-bold sm:inline-block">
+            <span className="font-bold">
               Novasuites
             </span>
           </Link>
@@ -44,7 +52,7 @@ export default function Header() {
             <Link href="/blog">Blog</Link>
           </nav>
         </div>
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+        <div className="flex flex-1 items-center justify-end space-x-2">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button variant="ghost" size="icon">
