13c6d03ac8f8696ea078f8efdecca9894618d3df
Test <testgyaan12@gmail.com>
2025-09-15 20:25:25 +0000

    Clean all

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,2 @@
 import { config } from 'dotenv';
 config();
-
-import '@/ai/flows/generate-article-from-prompt.ts';
-import '@/ai/flows/summarize-existing-article.ts';
-import '@/ai/flows/analyze-content-for-seo.ts';
